8fc6716c7d56d42ef3f4b2fa750734091b2c2536
Lauren Pak <>
2021-04-28 15:02:57 +0000

    Lauren Pak - Unit 8 Q1 Part A

--- Unit8_1a.java
+++ Unit8_1a.java
@@ -1,7 +1,20 @@
+//Name: Lauren Pak
+//Period: 1
+//Date: 4/27/21
 public class Unit8_1a
 {
   public int [] [] modifyColumns (int [] [] arr)
  { 
+   for(int r = 0; r < arr.length; r++)
+{
+     for(int c = 0; c < arr[0].length; c++)
+     {
+          if(c % 2 != 0)
+          {
+               arr[r][c] = arr[r][c-1];
+          }
+     }
+}
 return arr;
       }
     }
